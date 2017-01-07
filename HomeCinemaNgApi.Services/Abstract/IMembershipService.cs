b6bb4477dd1363db3b81604f12f7ea60358e7279
Sean <seon2you@hotmail.com>
2017-01-07 02:23:13 -0800

    working on membership

---
+++ HomeCinemaNgApi.Services/Abstract/IMembershipService.cs
@@ -0,0 +1,18 @@
+﻿using HomeCinemaNgApi.Entities;
+using HomeCinemaNgApi.Services.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeCinemaNgApi.Services.Abstract
+{
+    public interface IMembershipService
+    {
+        MembershipContext ValidateUser(string username, string password);
+        User CreateUser(string username, string email, string password, int[] roles);
+        User GetUser(int userId);
+        List<Role> GetUserRoles(string username);
+    }
+}
---
+++ HomeCinemaNgApi.Services/MembershipService.cs
@@ -0,0 +1,52 @@
+﻿using HomeCinemaNgApi.Services.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomeCinemaNgApi.Entities;
+using HomeCinemaNgApi.Services.Utilities;
+using HomeCinemaNgApi.Data.Repositories;
+using HomeCinemaNgApi.Data.Infrastructure;
+
+namespace HomeCinemaNgApi.Services
+{
+    public class MembershipService : IMembershipService
+    {
+        private readonly IEntityBaseRepository<User> _userRepository;
+        private readonly IEntityBaseRepository<Role> _roleRepository;
+        private readonly IEntityBaseRepository<UserRole> _userRoleRepository;
+        private readonly IEncryptionService _encryptionService;
+        private readonly IUnitOfWork _unitOfWork;
+        public MembershipService(IEntityBaseRepository<User> userRepository, IEntityBaseRepository<Role> roleRepository, IEntityBaseRepository<UserRole> userRoleRepository, IEncryptionService encryptionService, IUnitOfWork unitOfWork)
+        {
+            _userRepository = userRepository;
+            _roleRepository = roleRepository;
+            _userRoleRepository = userRoleRepository;
+            _encryptionService = encryptionService;
+            _unitOfWork = unitOfWork;
+        }
+
+        public User CreateUser(string username, string email, string password, int[] roles)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUser(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Role> GetUserRoles(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MembershipContext ValidateUser(string username, string password)
+        {
+            var membershipContext = new MembershipContext();
+
+            var user = _userRepository.GetSingleByUsername(username);
+        }
+    }
+}
