111d6f38c1725f511dd717faea87f36303331001
Sean <seon2you@hotmail.com>
2017-01-08 21:04:39 -0800

    working on angular sidemenu

--- HomeCinemaNgApi.Web/Scripts/spa/home/rootCtrl.js
+++ HomeCinemaNgApi.Web/Scripts/spa/home/rootCtrl.js
@@ -5,6 +5,16 @@
     rootCtrl.$inject = ['$scope', '$location', '$rootScope'];
 
     function rootCtrl($scope, $location, $rootScope) {
+        $scope.userData = {};
 
+        $scope.userData.displayUserInfo = displayUserInfo;
+        $scope.logout = logout;
+
+        function displayUserInfo() {
+
+        }
+        function logout() {
+
+        }
     }
 })(angular.module('homeCinemaNgApi'));
--- HomeCinemaNgApi.Web/Scripts/spa/layout/topBar.directive.js
+++ HomeCinemaNgApi.Web/Scripts/spa/layout/topBar.directive.js
@@ -1,1 +1,13 @@
-﻿
+﻿(function (app) {
+    'use strict';
+
+    app.directive('topBar', topBar);
+
+    function topBar() {
+        return {
+            restrict: 'E',
+            replace: true,
+            templateUrl: '/scripts/spa/layout/topBar.html'
+        }
+    }
+})(angular.module('common.ui'));
